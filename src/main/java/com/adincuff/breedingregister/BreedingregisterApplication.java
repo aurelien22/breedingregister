86603f46db637ff8329681e961056893b7208f82
aurelien22 <aurelien.dincuff@gmail.com>
2021-07-07 10:21:21 +0200

    Initial commit

---
+++ src/main/java/com/adincuff/breedingregister/BreedingregisterApplication.java
@@ -0,0 +1,13 @@
+package com.adincuff.breedingregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreedingregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreedingregisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adincuff/breedingregister/BreedingregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adincuff.breedingregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreedingregisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

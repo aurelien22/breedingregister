179572b426ab4edef3e02dfa16210eeb14cb9e9c
aurelien22 <aurelien.dincuff@gmail.com>
2021-07-12 10:46:34 +0200

    Tests refactoring

--- src/test/java/com/adincuff/breedingregister/User/UserControllerTest.java
+++ src/test/java/com/adincuff/breedingregister/User/UserControllerTest.java
@@ -2,10 +2,9 @@ package com.adincuff.breedingregister.User;
 
 import com.adincuff.breedingregister.Horse.*;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,14 +16,13 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     HorseService horseService;
 
     @Test
@@ -33,8 +31,7 @@ public class UserControllerTest {
         String userId = "217h391e810c19729de860ml";
 
         when(horseService.getHorsesByUser(userId)).thenReturn(List.of(
-                new Horse("507f191e810c19729de860ea", "exeko camara", Gender.GELDING, Color.BAY, Race.FRENCH_SADDLEBRED, LocalDate.of(2014, 06, 24), "217h391e810c19729de860ml"),
-                new Horse("507f191e810c10160rv860ui", "jackyria camara", Gender.FEMALE, Color.BAY, Race.FRENCH_SADDLEBRED, LocalDate.of(2019, 04, 11), "507f191e810c19723de860po")
+                new Horse("507f191e810c19729de860ea", "exeko camara", Gender.GELDING, Color.BAY, Race.FRENCH_SADDLEBRED, LocalDate.of(2014, 06, 24), "217h391e810c19729de860ml")
         ));
 
         mockMvc.perform(get("/api/user/217h391e810c19729de860ml/horses"))
